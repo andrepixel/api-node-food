f9cd5753a3fed0eaf282e062f33d770e40b53074
Source <devboxever17@gmail.com>
2023-01-18 16:40:10 -0300

    added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import * as express from 'express';
 import { routes } from './routes';
 import mongoose from 'mongoose';
-
+import * as env from 'dotenv';
 
 mongoose.set('strictQuery', false);
+
+env.config({ path: './.env'});
+
 mongoose.connect('mongodb://localhost:27017').then(() => {
-	const app = express.default();
-	const port = 3000;
+	const app = express();
 
 	app.use(express.json());
 	app.use(routes);
-	app.listen(port, () => console.log(`Running on http://localhost:${port}`));
+	app.listen(process.env.PORT, () => console.log(`Running on http://localhost:${process.env.PORT}`));
 }).catch(
 	(err) => console.log(err),
 );
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import { getProduct } from './modules/products/products.service';
 import { deleteCategories, getCategories, postCategories } from './modules/products/categories.service';
 
-export const routes = express.default();
+export const routes = express();
 
 routes.get('/categories', getCategories);
 routes.post('/categories', postCategories);
